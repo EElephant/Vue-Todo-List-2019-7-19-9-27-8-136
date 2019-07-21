85aefdc9b864bec01b1cb1a181de16fb22902d5b
Ethan <782227508@qq.com>
2019-07-21 23:25:36 +0800

    Feat: show the Todo List, and add Css for it

---
+++ todoList/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h2>Jquery To Do List</h2>
+    <h4>Simple Todo List with adding and filter bydiff status</h4>
+    <input id="inputText"/><button id="addButton">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#inputText{
+  width: 500px;
+  height: 30px;
+}
+h4{
+  font-style: italic;
+}
+#addButton{
+  width: 70px;
+  height: 40px;
+  color: white;
+  background-color: #fc999b;
+}
+</style>
